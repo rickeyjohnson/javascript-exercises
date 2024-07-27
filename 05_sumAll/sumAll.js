d31595e1bbeb1864ee52ea75b4a6c41ade88f525
rickeyjohnson <rdjwinter2004@gmail.com>
2024-07-27 11:58:19 -0400

    did js 5
    uggghhhh, but getting more effiecient

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0
+    let start
+    let end
+    let num1Type = typeof num1
+    let num2Type = typeof num2
 
+    if (num1Type !== 'number' || num2Type !== 'number') {
+        return 'ERROR'
+    }
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR'
+    }
+
+    if (num1 < 1 || num2 < 1) {
+        return 'ERROR'
+    }
+
+    if (num1 < num2) {
+        start = num1
+        end = num2
+    } else {
+        start = num2
+        end = num1
+    }
+
+    for (let i = start; i <= end; i++) {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
