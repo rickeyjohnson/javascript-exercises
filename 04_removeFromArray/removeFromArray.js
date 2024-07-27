5f86cb55f969f54d100124dc18754a1e59c56a78
rickeyjohnson <rdjwinter2004@gmail.com>
2024-07-27 11:32:45 -0400

    did remove from array js
    it was long, it was hard, didnt commit, but pushed through and did it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = (arr, ...items) => {
+
+    let indexes = []
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < items.length; j++) {
+            if (arr[i] === items[j]) {
+                indexes.push(arr.indexOf(items[j]))
+            }
+        }
+    }
+
+    for (let index of indexes.reverse()) {
+        arr.splice(index, 1)
+    }
+
+    return arr
 };
 
 // Do not edit below this line
