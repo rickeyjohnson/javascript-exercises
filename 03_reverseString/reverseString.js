719da636355db762540611a1073f5e48aad81cd5
rickeyjohnson <rdjwinter2004@gmail.com>
2024-07-27 10:14:13 -0400

    js 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = (str) => {
+    let reversed = ""
 
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str.charAt(i)
+    }
+
+    return reversed
 };
 
 // Do not edit below this line
