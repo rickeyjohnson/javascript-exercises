c59cfd27c2a31d7a058a4d3846e0ed38665a0abc
rickeyjohnson <rdjwinter2004@gmail.com>
2024-07-30 09:40:34 -0400

    have palidromes working with puncutation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,8 +2,25 @@ function isLetter (symbol) {
     return symbol.toLowerCase() != symbol.toUpperCase()
 }
 
+function strToArr(str) {
+    return str.toLowerCase()
+              .replace(" ", "")
+              .split('')
+              .filter((letter) => {
+                return isLetter(letter)
+              })
+}
+
 const palindromes = function (str) {
-    
+    let word = strToArr(str)
+
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] !== word[word.length - i - 1]) {
+            return false
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
