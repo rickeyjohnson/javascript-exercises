a1158ea293090e2a9451daef2c3e0848a7146193
rickeyjohnson <rdjwinter2004@gmail.com>
2024-07-30 10:26:54 -0400

    got basic concepts of fibonnici working

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = (num) => {
+    let result = 0
+    let sequenceOfNums = [0, 1]
 
+    for (let i = 2; i <= num; i++) {
+        sequenceOfNums.push(sequenceOfNums[i - 2] + sequenceOfNums[i - 1])
+    }
+
+    return sequenceOfNums[sequenceOfNums.length - 1]
 };
 
 // Do not edit below this line
