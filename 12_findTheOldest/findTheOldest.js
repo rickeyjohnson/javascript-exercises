8541685050342748d1d0bd2b69fee2ed2b0793fb
rickeyjohnson <rdjwinter2004@gmail.com>
2024-07-30 10:40:47 -0400

    got test 1 of oldest working

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = (people) => {
+    let oldestAge = 0
+    let oldestPerson
 
+    for (let person of people) {
+        let age = person.yearOfDeath - person.yearOfBirth
+        if (age > oldestAge) {
+            oldestAge = age
+            oldestPerson = person
+        }
+    }
+
+    return oldestPerson
 };
 
 // Do not edit below this line
